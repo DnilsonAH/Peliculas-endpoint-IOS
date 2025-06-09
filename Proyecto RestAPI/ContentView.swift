2f93cdcacdec3e5c0bddfdbab137e0e698928e82
DnilsonAH <dnilson.achahuanco@tecsup.edu.pe>
2025-06-09 16:30:06 -0500

    Initial Commit

---
+++ Proyecto RestAPI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Proyecto RestAPI
+//
+//  Created by MAC14 on 9/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Proyecto RestAPI/Proyecto_RestAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_RestAPIApp.swift
+//  Proyecto RestAPI
+//
+//  Created by MAC14 on 9/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_RestAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
